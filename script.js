6fd62713f535a3f150a8fb29a5a5919c6c93f774
Yaslam Salman Sando <123530420+salamsando@users.noreply.github.com>
2025-11-12 15:30:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ window.onload = function() {
 document.getElementById("myButton").addEventListener("click", function() {
     document.getElementById("info").textContent = "You just clicked the button!";
 });
-var name = "Yaslam Salman Sando";
-var favouriteColor = "Aqua Green";
+const name = "Yaslam Salman Sando";
+const favouriteColor = "Aqua Green";
 const currentYear = new Date().getFullYear();
 
 console.log(`Name: ${name}`);
 console.log('Favourite Color: ${favouriteColor}');
 console.log('Current Year: ${currentYear}');
+
 
