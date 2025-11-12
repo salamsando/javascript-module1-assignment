8f9f2803a35a71147df0afba6dfc42ed8a18140e
Yaslam Salman Sando <123530420+salamsando@users.noreply.github.com>
2025-11-12 15:23:50 +0000

    update
    Added variables for name and favorite color, and logged them to the console.

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ window.onload = function() {
 document.getElementById("myButton").addEventListener("click", function() {
     document.getElementById("info").textContent = "You just clicked the button!";
 });
+var name = "Yaslam Salman Sando";
+var favouriteColor = "Aqua Green";
+const currentYear = new Date().getFullYear();
+
+console.log(`Name: ${name}`);
+console.log('Favourite Color: ${favouriteColor}');
+console.log('Current Year: ${currentYear}');
+
